d3f949e5f1cd3a17704973258ea751623ef44750
Xatasan <xatasan@firemail.cc>
2018-02-04 14:02:57 +0100

    Created "mirror.sh" script wrap and configure the makefile

--- Makefile
+++ Makefile
@@ -1,69 +1,28 @@
-PANDOC_OPT=-s -f markdown-raw_html --data-dir=./res/ --template=./res/default
-ifdef BRAND
-PANDOC_OPT+=-V brand
-endif
-PANDOC_EPUB_OPT=$(PANDOC_OPT) -m --epub-chapter-level=2 -t epub3
-PANDOC_HTML_OPT=$(PANDOC_OPT) --katex --html-q-tags --css=../style.css -t html5
-CALIBRE_MOBI_OPT=--pretty-print --enable-heuristics
-PANDOC_XETEX_OPT=$(PANDOC_OPT) -V papersize=a4 -V fontsize=12pt -V geometry="margin=1.2in" -V documentclass=article -V mainfont="Utopia" --latex-engine=xelatex -t latex
-PANDOC_MS_OPT=$(PANDOC_OPT) -V papersize=a4 -V fontfamily=N -V hyphenate=1 -t ms
+.SUFFIXES: .md .html .epub .mobi .pdf
+PD_OPT=-s -f markdown-raw_html --data-dir=./res/ --template=./res/default -V NAME="$(M_NAME)" -V WMAST="$(M_WMASTER)"
 
-DIRS=epub mobi html pdf
-TEXTS:=$(wildcard txt/*.md)
-EPUB=$(TEXTS:txt/%.md=epub/%.epub)
-MOBI=$(TEXTS:txt/%.md=mobi/%.mobi)
-HTML=$(TEXTS:txt/%.md=html/%.html)
-PDF=$(TEXTS:txt/%.md=pdf/%.pdf)
-
-index.html: html keywords.html style.css
+index.html: keywords.html
 	find txt/ -name "*.md" -print0 |\
-			xargs -L 1 -P 4 -0 awk --posix -f res/md.awk |\
+			xargs -L 1 -P 4 -0 awk -f res/md.awk |\
 			sort -t'	' -k1,1 -k3,3n -k2,2 |\
-			awk --posix -f res/idauth.awk -f res/index.awk > index.html
+			awk -f res/idauth.awk -f res/index.awk -v NAME="$(M_NAME)" -v WMAST="$(M_WMASTER)" > index.html
 
 keywords.html:
 	find txt/ -name "*.md" -print0 |\
-			xargs -L 1 -P 4 -0 awk --posix -f res/md.awk -v keywords=1 |\
+			xargs -L 1 -P 4 -0 awk -f res/md.awk -v keywords=1 |\
 			sort -t'	' -k5,5 -k1,1 -k3,3n -k2,2 |\
-			awk --posix -f res/idauth.awk -f res/keywords.awk > keywords.html
+			awk -f res/idauth.awk -f res/keywords.awk -v NAME="$(M_NAME)" -v WMAST="$(M_WMASTER)" > keywords.html
 
-$(DIRS:%=mkdir-%):
-	mkdir -p $(patsubst mkdir-%,%,$@)
+.md.html:
+	awk -f res/idauth.awk -f res/prehtml.awk $< |\
+		pandoc $(PD_OPT) --katex --html-q-tags --css=../style.css -t html5 |\
+		awk -f res/idauth.awk -f res/posthtml.awk > $@
 
-epub: mkdir-epub $(EPUB) 
-	rm -f index.htmk keywords.html
-	$(MAKE) index.html
-epub/%.epub: txt/%.md 
-	pandoc $< $(PANDOC_EPUB_OPT) -o $@
+.md.epub:
+	pandoc $< $(PD_OPT) --mathml --epub-chapter-level=2 -t epub3 -o $@
 
-mobi: mkdir-mobi $(MOBI) 
-	rm -f index.htmk keywords.html
-	$(MAKE) index.html
-mobi/%.mobi: epub/%.epub 
-	ebook-convert $< $@ $(CALIBRE_MOBI_OPT)
+.epub.mobi:
+	ebook-convert $< $@ --pretty-print --enable-heuristics
 
-html: mkdir-html $(HTML)
-html/%.html: txt/%.md
-	awk --posix -f res/idauth.awk -f res/prehtml.awk $< |\
-		pandoc $(PANDOC_HTML_OPT) |\
-		awk --posix -f res/idauth.awk -f res/posthtml.awk > $@
-
-pdf: mkdir-pdf $(PDF) 
-	rm -f index.htmk keywords.html
-	$(MAKE) index.html
-pdf/%.pdf: txt/%.md
-ifdef MS
-	pandoc $< $(PANDOC_MS_OPT) -o $@
-else
-	pandoc $< $(PANDOC_XETEX_OPT) -o $@
-endif
-
-all: epub mobi pdf html index.html
-
-style.css:
-	cp ./res/style.css .
-
-clean:
-	rm -rf $(DIRS) index.html keywords.html style.css
-
-.PHONY: all clean mkdir-epub mkdir-mobi mkdir-html mkdir-pdf 
+.md.pdf:
+	pandoc $< -V papersize=a4 $(PD_OPT) --pdf-engine=$(PDF_ENG) -o $@
---
+++ mirror.sh
@@ -0,0 +1,172 @@
+#!/bin/sh
+# redtexts "mirror tool" makescript
+# by xat [https://sub.god.jp/~xat]
+# in the public domain, 2018-
+
+# colours
+bold="$(tput bold)"
+red="$(tput setaf 1)"
+reset="$(tput sgr0)"
+export bold red reset
+
+# constants and defaults
+MCONF="./.mconf"
+D_TXT="./txt/"
+D_HTML="./html/"
+D_EPUB="./epub/"
+D_MOBI="./mobi/"
+D_PDF="./pdf/"
+
+# functions
+errcho() {
+	echo "${bold}${red}ERROR:${reset} $*" 1>&2
+	exit 1
+}
+
+_runconf() {
+	[ -e $MCONF ] && mv $MCONF $MCONF.old
+	rm -f  .auto
+	echo "# please do not edit by hand" > $MCONF
+
+	printf "Mirror name: "
+	read -r NAME
+	echo "M_NAME=\"$NAME\"" >> $MCONF
+
+	printf "Webmaster URL (leave empty for no webmaster): "
+	read -r WMASTER
+	[ "$WMASTER" ] && echo "M_WMASTER=\"$WMASTER\"" >> $MCONF
+
+	printf "Remote destination (optional): "
+	read -r REMOTE
+	[ "$REMOTE" ] && {
+		echo "M_REMOTE=\"$REMOTE\"" >> $MCONF
+
+		printf "Automatically synchronise [Y/n]? "
+		read -r AUTOSYNC
+		echo "$AUTOSYNC" | grep -vi "^n" 2>/dev/null >/dev/null &&\
+			echo "O_AUTOSYNC=1" >> $MCONF
+	}
+
+	printf "Copy instead of linking generated files [Y/n]? "
+	read -r COPY
+	if echo "$COPY" | grep -vi "^n"; then
+		echo "C_COPY=\"cp\"" >> $MCONF
+	else
+		echo "C_COPY=\"ln -srf\"" >> $MCONF
+	fi
+
+	printf "Generate EPUB files [Y/n]? "
+	read -r GEN_E
+	echo "$GEN_E" | grep -vi "^n" 2>/dev/null >/dev/null && {
+		echo 'G_EPUB=1' >> $MCONF
+
+		which ebook-convert >/dev/null 2>/dev/null && {
+			# mobi files can only be generated if epub files are generated AND
+			# `ebook-convert` is installed
+			printf "Generate MOBI (Kindle) files [Y/n]? "
+			read -r GEN_M
+			echo "$GEN_M" | grep -vi "^n" 2>/dev/null >/dev/null &&\
+				echo 'G_MOBI=1' >> $MCONF
+		}
+	}
+
+	printf "Generate PDF files [Y/n]? "
+	read -r GEN_P
+	echo "$GEN_P" | grep -vi "^n" 2>/dev/null >/dev/null &&{
+		echo 'G_PDF=1' >> $MCONF
+
+		printf "Use Groff (-ms) to generate the PDFs [Y/n]? "
+		read -r USE_G
+		echo "$USE_G" | grep -vi "^n" 2>/dev/null >/dev/null && {
+			echo 'O_MS=1' >> $MCONF
+		}
+	}
+
+	printf "Autorun this configuration from now on [y/N]? "
+	read -r AUTOR
+	echo "$AUTOR" | grep -vi "^n" 2>/dev/null >/dev/null && touch .auto
+}
+
+_getfiles() {
+	find $D_TXT -name "*.md" -type f |\
+		sed "s%\\.md$%$1%;s%$% %g"
+}
+
+# test if all necessary binaries are available
+which make >/dev/null 2>&1 || errcho "\"make\" not installed!"
+which pandoc >/dev/null 2>&1 || errcho "\"pandoc\" not installed!"
+which awk >/dev/null 2>&1 || errcho "\"awk\" not installed!"
+
+# start setup (ensure $MCONF exists and then load it)
+# echo "${bold}::: mirror.sh - create, maintain and update a redtexts mirror${reset}"
+if [ ! -e .auto ]; then
+	if [ -e $MCONF ]; then
+		printf "Previous configuration file found, do you want to use it again [Y/n]? "
+		read -r PCONF
+		echo "$PCONF" | grep -i "^n" 2>/dev/null >/dev/null && _runconf
+	else
+		_runconf
+	fi
+fi
+. $MCONF
+
+# check if variables were properly loaded
+[ "$C_COPY" ] || [ "$M_NAME" ] || errcho "there was an error in \"$MCONF\". Some necessary variables weren't specified"
+
+
+# construct argument list for make
+FILES="$(_getfiles .html)"
+
+[ $G_MOBI ] && FILES="$(_getfiles .mobi) $FILES"
+[ $G_EPUB ] && FILES="$(_getfiles .epub) $FILES"
+if [ $G_PDF ]; then
+	FILES="$(_getfiles .pdf) $FILES"
+	if [ $O_MS ]; then
+		PDF_ENG="pdfroff"
+	else
+		PDF_ENG="xelatex"
+	fi
+fi
+
+# prepare and run make
+mkdir -p $D_HTML
+[ $G_EPUB ] && mkdir -p $D_EPUB
+[ $G_MOBI ] && mkdir -p $D_MOBI
+[ $G_PDF ] && mkdir -p $D_PDF
+make "${@:--s}" M_NAME="$M_NAME" M_WMASTER="$M_WMASTER"\
+	 PDF_ENG="$PDF_ENG" $FILES keywords.html index.html || exit 1
+
+# copy or link files from build dir to intended location
+for d in $D_TXT/*.html; do
+	$C_COPY "$PWD/$d" "$D_HTML"
+done
+
+if [ $G_EPUB ]; then
+	for d in $D_TXT/*.epub; do
+		$C_COPY "$PWD/$d" "$D_EPUB"
+	done
+fi
+
+if [ $G_MOBI ]; then
+	for d in $D_TXT/*.mobi; do
+		$C_COPY "$PWD/$d" "$D_MOBI"
+	done
+fi
+
+if [ $G_PDF ]; then
+	for d in $D_TXT/*.pdf; do
+		$C_COPY "$PWD/$d" "$D_PDF"
+	done
+fi
+
+# automatically synchronise if
+if [ $O_AUTOSYNC ]; then
+	if which rsync 2>/dev/null >/dev/null; then
+		rsync -Lavcu $FILES $M_REMOTE
+	elif which scp 2>/dev/null >/dev/null; then
+		scp -Crp $FILES $M_REMOTE
+	else
+		sed -i '/O_AUTOSYNC/d'
+		errcho "Couldn't automatically sync. Option removed."
+	fi
+fi
